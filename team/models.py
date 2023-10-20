206ca3a7fdda1802eeb2410af1e858189d6bff87
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-10-20 18:25:16 +0300

    Created Team model.

--- team/models.py
+++ team/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    employees = models.ManyToManyField(to=get_user_model(), related_name="teams")
+
+    def __str__(self):
+        return self.name
