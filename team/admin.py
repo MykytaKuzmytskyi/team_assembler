b56acfc3f183d9f55435dfaedb3a000a922d5dee
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-10-20 20:49:49 +0300

    Update admin panels.

--- team/admin.py
+++ team/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .models import Team
 
-from team.models import Team
 
-admin.site.register(Team)
+class EmployeeInline(admin.TabularInline):
+    model = Team.employees.through
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['name', 'employee_count', 'employee_names']
+    list_filter = ['employees']
+    search_fields = ['name']
+    filter_horizontal = ('employees',)
+    inlines = [EmployeeInline]
+
+    def employee_count(self, obj):
+        return obj.employees.count()
+
+    employee_count.short_description = 'Number of Employees'
+
+    def employee_names(self, obj):
+        return ", ".join([employee.get_full_name() for employee in obj.employees.all()])
+
+    employee_names.short_description = 'Employee Names'
