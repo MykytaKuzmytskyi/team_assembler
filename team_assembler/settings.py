ae0dbba4c628ec05c4bc2cd7780182af98bd35da
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-10-20 18:20:22 +0300

    Create EmployeeSerializer.

--- team_assembler/settings.py
+++ team_assembler/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "employee",
 ]
 
@@ -81,6 +82,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "employee.Employee"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
