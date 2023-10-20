e852e9c4e9894e85668ea596e3ecaa51f7e90cc4
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-10-20 18:27:18 +0300

    Created Serializers for Team model.

---
+++ team/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from team.models import Team
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = ['id', 'name', 'employees']
+
+
+class TeamListSerializer(TeamSerializer):
+    employees = serializers.StringRelatedField(many=True)
+
+
+class TeamAddRemoveEmployeesSerializer(serializers.Serializer):
+    employee_ids = serializers.ListField(
+        required=True,
+    )
