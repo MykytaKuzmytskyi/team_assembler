69233a3215230d5bd571c993cc8f17619be8aabc
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-10-20 18:21:17 +0300

    Created views for CRUD system of Employee model.

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Employee
+from .serializers import EmployeeSerializer
+
+
+class CreateEmployeeView(generics.CreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class EmployeeListView(generics.ListAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class EmployeeDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
